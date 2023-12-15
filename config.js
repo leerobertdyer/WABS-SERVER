255140ffde112aac5f20db8fa4cc0145b31b565b
leerobertdyer <leerobertdyer@gmail.com>
2023-12-15 15:34:18 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+export const ELEPHANTSQL_URL = process.env.ELEPHANTSQL_URL
+export const DROPBOX_APP_KEY = process.env.DROPBOX_APP_KEY
+export const DROPBOX_APP_SECRET = process.env.DROPBOX_APP_SECRET
+export const REACT_APP_FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL
