9e432c744cd013a32f2b26353a54ef0800879469
leerobertdyer <leerobertdyer@gmail.com>
2024-01-23 22:48:38 -0500

    updated prompts to include better and more random ones

--- routes/promptRoutes.js
+++ routes/promptRoutes.js
@@ -10,11 +10,28 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
 promptRoutes.post('/get-prompt', async (req, res) => {
   const userId = req.body.userId
-  const prompt = `You are a creative song prompt builder. You use a variety of topics to furnish short thoughtful song prompts that can later be checked against lyrics to see wether a user has followed the prompt or not. Pull from a wide variety of themes including but not limited to: Nature, Nostalgia, Romance, Family, Future, Other Environments, Fantasy, Sci-Fi, Silliness, Seriousness, Sadness. Only give one prompt at a time.`
+  const promptHeader = 'You are a creative song prompt generator. Here are your guidelines: -Only give one prompt. '
+  const promptThemes = '-Use one of a variety of themes, including: a) Emotional (e.g., love, sadness, fury, peace, rebellion, wonder) b) Abstract (e.g., time, the nature of reality, Dreamscape, Chaos/Order, Metamorphosis) c) Genre-Based (e.g., sci-fi, fantasy, alternative history, ghost story, aliens) d) Mystical (e.g., the hidden, the unknown, the impossible, the absurd, Surreal). -Make them as original as possible. -You will be checking the songs users generate in the future so make sure the prompt is something that you can check against. '
+  let prompt = ''
+  const randNum = Math.ceil(Math.random()*10)
+  if (randNum < 6) {
+    console.log('Basic Prompt')
+    prompt = promptHeader + promptThemes + 'KEEP THE ACTUAL PROMPT CONCISE using one sentence, and use only one idea at a time.'
+  } else if (randNum > 5) {
+    if (randNum < 9) {
+      console.log('Title Prompt')
+      prompt = promptHeader + promptThemes + 'Create a prompt that is only the title, between one and 8 words. Make sure to start with "Write a song with the title: "'
+    }
+    else {
+      console.log('word length prompt')
+      prompt = promptHeader + 'Use a specifiic word length to restrict the songwriter. It may be less than a certain amount or more than a certain amount. DO NOT ask them to write more than 110 words. DO NOT ask them to write a song with less than 2 words. KEEP THE PROMPT SHORT AND ONLY USE THE WORD LENGTH. DO NOT EMBELISH.'
+    }
+  }
+
+
   try {
     const prompts = await db('prompt').select('prompt').where('user_id', userId).whereRaw('EXTRACT(MONTH FROM created) = EXTRACT(MONTH FROM CURRENT_DATE)')
-    if (prompts && prompts.length < 2) {
-
+    if (prompts.length < 2) {
       try {
         const stream = await openai.chat.completions.create({
           model: 'gpt-4',
@@ -100,7 +117,7 @@ promptRoutes.put('/check-prompt', async (req, res) => {
         await trx('prompt').where('user_id', userId).where('prompt', prompt).del();
 
       })
-      io.emit('updateFeed')
+      io.emit('updateFeed') //? why update feed here ?//
 
       res.status(200).json({ gptResponse: gptResponse.slice(4), pass: true })
     } else if (gptResponse.slice(0, 5).toLowerCase().includes('no')) {
